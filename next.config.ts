4c9cb2458811f8c5bf4bb15f3e18b28924d37ceb
ozodn <ozodnx@gmail.com>
2025-02-19 22:17:21 +0500

    localization

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+const createNextIntlPlugin = require('next-intl/plugin');
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withNextIntl(nextConfig);
